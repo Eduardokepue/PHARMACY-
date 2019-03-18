33ead2b043e89616b0facc1dd68b508f98e4b7ef
Eduardokepue <eduardokepue@gmail.com>
2019-03-18 11:51:29 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+Pharmcare
+</title>
+    <link rel="shortcut icon" href="admin/images/pos.jpg">
+
+  <link href="admin/css/bootstrap.css" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="admin/css/DT_bootstrap.css">
+  
+  <link rel="stylesheet" href="admin/css/font-awesome.min.css">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+      .sidebar-nav {
+        padding: 9px 0;
+      }
+    </style>
+    <link href="admin/css/bootstrap-responsive.css" rel="stylesheet">
+
+<link href="style.css" media="screen" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <div class="container-fluid">
+      <div class="row-fluid">
+		<div class="span4">
+		</div>
+	
+</div>
+<div id="login">
+
+<form action="login.php" method="post">
+
+			<font style=" font:bold 44px 'Aleo'; text-shadow:1px 1px 15px #000; color:#fff;"><center>Pharmcare</center></font><br>
+		
+<div class="input-prepend">
+		<span style="height:30px; width:25px;" class="add-on"><i class="icon-user icon-2x"></i></span><input style="height:30px;" type="text" name="username" Placeholder="Username" required/><br>
+</div>
+<div class="input-prepend">
+	<span style="height:30px; width:25px;" class="add-on"><i class="icon-lock icon-2x"></i></span><input type="password" style="height:30px;" name="password" Placeholder="Password" required/><br>
+		</div>
+    <div class="input-prepend">
+    <span style="height:30px; width:25px;" class="add-on"><i class="icon-user icon-2x"></i></span>
+<select name="position" style="height:40px;" >
+<option value="pharmacist" >pharmacist</option>
+<option value="cashier">cashier</option>
+ 
+</select>
+
+
+    </div>
+		<div class="qwe">
+		 <button class="btn btn-large btn-primary btn-block pull-right"  type="submit" value="login" name="submit" ><i class="icon-signin icon-large"></i> Login</button>
+</div>
+		 </form>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
+
